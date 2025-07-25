315f7ee2827e78e295e6a90605e4f6c074f20631
padme-004 <shenoy.nidhi004@gmail.com>
2025-07-26 03:29:04 +0530

    Test cases for problem1_test.py

--- problem1_test.py
+++ problem1_test.py
@@ -1,2 +1,13 @@
 #Run this file to check if the solution in problem1.py passes the following test cases:
 #nums=[[3, 2, 3, 9], [1, 2, 3, 4], [1, 2, 3, -1], [1, 2, -3, 4], [1, 1, 1, 3, 3, 4, 3, 2, 4, 2], [0, 0, 0, 1]]
+
+from problem1 import containsDuplicate1, containsDuplicate2
+#I will use containsDuplicate2 here
+
+tests=[[3, 2, 3, 9], [1, 2, 3, 4], [1, 2, 3, -1], [1, 2, -3, 4], [1, 1, 1, 3, 3, 4, 3, 2, 4, 2], [0, 0, 0, 1]]
+expected_ans=[[True],[False], [False], [False], [True], [True]]
+for i in range(len(tests)):
+    print("Test case: ", tests[i])
+    print("Expected answer: ", expected_ans[i])
+    print("Answer given by candidate's solution: ")
+    print(containsDuplicate2(tests[i]), end="\n\n")
